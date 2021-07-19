5eecd42a60d6e0ad875ae1c16acf08aa3a3fcfb0
Valeri <csvalkata@gmail.com>
2021-07-19 15:46:54 +0300

    themes and aside components html and css

--- Workshop_Components/forum-app/src/app/app.module.ts
+++ Workshop_Components/forum-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent
   ],
   imports: [
     BrowserModule,
